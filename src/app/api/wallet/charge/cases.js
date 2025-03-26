ed1bf22b8b6dcb14293d32c25732e340bae9785b
24HP9798 24HP9798 <24HP9798@admins-iMac.local>
2025-03-26 18:42:43 +0800

    feat: wallet and skins added

---
+++ src/app/api/wallet/charge/cases.js
@@ -0,0 +1,33 @@
+export const cases = [
+  {
+    id: 1,
+    name: "Canon Event",
+    price: 11.93,
+    image:
+      "https://cfdn.skin.club/iixekz9zfm/e2b27656653d6663eacc3b1785ebf26e0010ac88.png?policy=case-xl&dpr=2",
+  },
+
+  {
+    id: 2,
+    name: "Steel Samurai",
+    price: 40.36,
+    image:
+      "https://cfdn.skin.club/iixekz9zfm/11bdceac512129449cd38d9e5d7562e3bb1eea28.png?policy=case-xl&dpr=2",
+  },
+
+  {
+    id: 3,
+    name: "Revolver",
+    price: 3.17,
+    image:
+      "https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXU5A1PIYQNqhpOSV-fRPasw8rsUFJ5KBFZv668FFYwnfKfcG9HvN7iktaOkqD1auLTxD5SvZYgiLvFpo7xjVLh-kdrYWnzcoGLMlhpsyM-5vg/360fx360f",
+  },
+
+  {
+    id: 4,
+    name: "Chroma",
+    price: 5.42,
+    image:
+      "https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXU5A1PIYQNqhpOSV-fRPasw8rsUFJ5KBFZv668FFEuh_KQJTtEuI63xIXbxqOtauyClTMEsJV1jruS89T3iQKx_BBqa2j3JpjVLFH1xpp0EQ/360fx360f",
+  },
+];
--- src/app/api/wallet/charge/skins.js
+++ src/app/api/wallet/charge/skins.js
@@ -1,20 +1,271 @@
  export const skins = [
- {id:1,name:"Butterfly Knife | Crimson Web",status:"Factory New",price:1609,image:"https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpovbSsLQJf0ebcZThQ6tCvq4iSqODxMajum25V4dB8xL6Y9ImljQO3-UA_NmvzJoaTJFJsNAuE-AW4lOjqjce1upXAznZg7CA8pSGK_bJRSlU/360fx360f"}
- ,{id:2, name:"Bayonet knife | Doppler", status:"Factory New", price:800, image:"https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpotLu8JAllx8zJfAJG48ymmIWZqOf8MqjUxVRd4cJ5nqfDo9-m0Azm-Upsaz31LYSUcAU5Y16Crlm6wujmgJK_vZ3JnSAx6yAh-z-DyLGLsdXe/360fx360f"}
- ,{id:3, name:"Skeleton knife | Crimson Web", status:"Factory New", price:773, image:"https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpovbSsLQJfwPjNfThW49KJnJm0gPL2IITdn2xZ_Ish2O2S8dygjgfg-UptYT_6cIfGcg5oZFmDrgTtybzvhMS_v5zLmHU1pGB8st-DMg-F/360fx360f"}
- ,{id:4, name:"Flip knife | Doppler", status:"Factory New", price:638, image:"https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpovbSsLQJf1f_BYQJD4eOym5Cbm_LmDKvZl3hUufp9g-7J4cKti1Hm8xJkZ22lcNKSIwE6NQnUr1S7xLu5hZ676MiYnXtmuSBx537bgVXp1gHAfnds/360fx360f"}
- ,{id:5, name:"Stiletto knife | Crimson Web", status:"Field-Tested", price:536, image:"https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpovbSsLQJfwOfBfThW-NOJnJm0gPL2IITck29Y_cg_3ryUrY6i2Qbk-UdrYmvyJdXGJFRrZFjQ-AW_lLvv1sDp6JnPzSBrvT5iuyhlB3LdaA/360fx360f"}
- ,{id:6, name:"Desert eagle | Fennec Fox", status:"Factory New", price:492, image:"https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgposr-kLAtl7PTbTjlH7du6kb-Nkvn6NrjummJW4NE_2LqXodykigft80duMT2ido6XdQE9YFuBrwTqweu9gp-17Z2bwSRjvT5iuyiYgUnRyQ/360fx360f"}
- ,{id:7, name:"Bayonet knife | Damascus Steel", status:"Minimal Wear", price:361, image:"https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpotLu8JAllx8zJYAJG7dG3h4OehMj4OrzZgiVT7Zwj2brFoN_3iVC1qhBqZW2mJYDBcgU8YlnUrFLtxLrqg8fttMjJ1zI97X881rR_/360fx360f"}
- ,{id:8, name:"Specialist Glove | Crimson Web", status:"Field-Tested", price:318, image:"https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DAQ1h3LAVbv6mxFABs3OXNYgJR_Nm1nYGHnuTgDKzUlHhu-sB1teTE8YXghRq3-kZtZz36II-cew5saAyErlC2wru5hMe_vs7KyHNguSRz4S3azUfln1gSOVoikF85/360fx360f"}
- ,{id:9, name:"Huntsman knife | Autotronic", status:"Field-Tested", price:289, image:"https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpovbSsLQJfx_LLZTRB7dCJk5O0n-L6J6jcl2Vu6dBlhf3T-oL8i2u5rhc1JjTtI9KWIVNtYw6D_VC7x7-9g5PpuJrNy3th7HUg7XiIzRezhkpFauM6geveFwsLIcG8XQ/360fx360f"}
- ,{id:9, name:"AK-47 | Neon Rider", status:"Field-Tested", price:47, image:"https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpot7HxfDhjxszJegJM6dO4q5KCk_LmDLbUkmJE5Ytz0r6U8Y_ziVHn-UY5MT-icIWRJlJoYFnTr1W-lbjrh8Xtu8vAmHY3pGB8sth0zE2w/360fx360f"}
- ,{id:10, name:"Glock-18 | Neo-noir", status:"Minimal Wear", price:27, image:"https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgposbaqKAxf0Ob3djFN79eJmo-Chcj5Nr_Yg2Yf7pEniL2VoNql0Abt_0VvYW2nJ9PBdQdqMwrWrwC6lbrmjJK7uszOnWwj5Hcq7I0xkg/360fx360f"}
- ,{id:11, name:"AK-47 | Ice coaled", status:"Factory New", price:19, image:"https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpot7HxfDhjxszJemkV09-5k5SDnvnzIITdn2xZ_ItyiL3Dod6ii1DmqENrMD-nd9OXcAQ5YFHTrFm4w-u915K9v5WczHBrpGB8shQJGVI3/360fx360f"}
- ,{id:12, name:"AWP | Chromatic-Aberration", status:"Field-Tested", price:10, image:"https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpot621FAR17PLfYQJB5M65mYG0h_76OITck29Y_cg_3ruZo9nziwXj-EBkN2zxdtSRJlRqZFHWqFe3yeu90ZC-v5mYzSFk7j5iuygAeEUWVQ/360fx360f"}
- ,{id:13, name:"M4A4 | Tooth Fairy", status:"Field-Tested", price:3, image:"https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpou-6kejhjxszFJTwW082jkYWFkfb9IaLum25V4dB8xLqSrY3w3FLj_0A5amqiJtfDcVM_M17T8gK2k7-7jZG16JTBznpn6CU8pSGKsR9Xpxw/360fx360f"}
- ,{id:14, name:"Galil AR | Chromatic-Aberration", status:"Battle-Scarred", price:2, image:"https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgposbupIgthwczLZAJF7dC_mL-In-X7PrruhmJf4_p5j-jX7ML33Aa3_0RpYW6ictPHIAc6NA7Y81G-kOzv05_tvsyanCBkuiUrsHfdgVXp1mIEZeU0/360fx360f"}
- ,{id:15, name:"Dual-Berettas | Twin Turbo", status:"Field-Tested", price:2, image:"https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpos7asPwJf0Ob3dShD4OOzmImfkuTLIbrdmnJu5cB1g_zMu9it3gW28ktqMWn7JdSTewI9NwrUqwW3wrrmgZa76Z-fznAy73Im4ynD30vgwyIuzrs/360fx360f"}
- ,{id:16, name:"Glock-18 | Teal-Graf", status:"Factory New", price:2, image:"https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgposbaqKAxf2-r3fitH_sy3h5O0g_bzNL7VqX9U6clOhuDG_Zi73wzj80Q5NzzyLYWcdlI5ZlnR-VW6xr-5hcS1vZWYmnUwvikntH-Mlgv3308MHHmPJA/360fx360f"}
- ,{id:17, name:"M4A1-S | Night Terror", status:"Factory New", price:1, image:"https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpou-6kejhz2v_Nfz5H_uOxh7-Gw_alIITYmHhe5ct4i9bN_Iv9nBqyqRE_MmD3JIaVdgFrNwzX_1LrwLjoh8Xu6p3Iy3BnuCEq7XrdmRS-n1gSOUNQ8BIA/360fx360f"}
-]
+   {
+     id: 1,
+     name: "Butterfly Knife | Crimson Web",
+     status: "Factory-New",
+     price: 1609,
+     image:
+       "https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpovbSsLQJf0ebcZThQ6tCvq4iSqODxMajum25V4dB8xL6Y9ImljQO3-UA_NmvzJoaTJFJsNAuE-AW4lOjqjce1upXAznZg7CA8pSGK_bJRSlU/360fx360f",
+   },
+   {
+     id: 2,
+     name: "Bayonet knife | Doppler",
+     status: "Factory-New",
+     price: 800,
+     image:
+       "https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpotLu8JAllx8zJfAJG48ymmIWZqOf8MqjUxVRd4cJ5nqfDo9-m0Azm-Upsaz31LYSUcAU5Y16Crlm6wujmgJK_vZ3JnSAx6yAh-z-DyLGLsdXe/360fx360f",
+   },
+   {
+     id: 3,
+     name: "Skeleton knife | Crimson Web",
+     status: "Factory-New",
+     price: 773,
+     image:
+       "https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpovbSsLQJfwPjNfThW49KJnJm0gPL2IITdn2xZ_Ish2O2S8dygjgfg-UptYT_6cIfGcg5oZFmDrgTtybzvhMS_v5zLmHU1pGB8st-DMg-F/360fx360f",
+   },
+   {
+     id: 4,
+     name: "Flip knife | Doppler",
+     status: "Factory-New",
+     price: 638,
+     image:
+       "https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpovbSsLQJf1f_BYQJD4eOym5Cbm_LmDKvZl3hUufp9g-7J4cKti1Hm8xJkZ22lcNKSIwE6NQnUr1S7xLu5hZ676MiYnXtmuSBx537bgVXp1gHAfnds/360fx360f",
+   },
+   {
+     id: 5,
+     name: "Stiletto knife | Crimson Web",
+     status: "Field-Tested",
+     price: 536,
+     image:
+       "https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpovbSsLQJfwOfBfThW-NOJnJm0gPL2IITck29Y_cg_3ryUrY6i2Qbk-UdrYmvyJdXGJFRrZFjQ-AW_lLvv1sDp6JnPzSBrvT5iuyhlB3LdaA/360fx360f",
+   },
+   {
+     id: 6,
+     name: "Desert eagle | Fennec Fox",
+     status: "Factory-New",
+     price: 492,
+     image:
+       "https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgposr-kLAtl7PTbTjlH7du6kb-Nkvn6NrjummJW4NE_2LqXodykigft80duMT2ido6XdQE9YFuBrwTqweu9gp-17Z2bwSRjvT5iuyiYgUnRyQ/360fx360f",
+   },
+   {
+     id: 7,
+     name: "Bayonet knife | Damascus Steel",
+     status: "Minimal-Wear",
+     price: 361,
+     image:
+       "https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpotLu8JAllx8zJYAJG7dG3h4OehMj4OrzZgiVT7Zwj2brFoN_3iVC1qhBqZW2mJYDBcgU8YlnUrFLtxLrqg8fttMjJ1zI97X881rR_/360fx360f",
+   },
+   {
+     id: 8,
+     name: "Specialist Glove | Crimson Web",
+     status: "Field-Tested",
+     price: 318,
+     image:
+       "https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DAQ1h3LAVbv6mxFABs3OXNYgJR_Nm1nYGHnuTgDKzUlHhu-sB1teTE8YXghRq3-kZtZz36II-cew5saAyErlC2wru5hMe_vs7KyHNguSRz4S3azUfln1gSOVoikF85/360fx360f",
+   },
+   {
+     id: 9,
+     name: "Huntsman knife | Autotronic",
+     status: "Field-Tested",
+     price: 289,
+     image:
+       "https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpovbSsLQJfx_LLZTRB7dCJk5O0n-L6J6jcl2Vu6dBlhf3T-oL8i2u5rhc1JjTtI9KWIVNtYw6D_VC7x7-9g5PpuJrNy3th7HUg7XiIzRezhkpFauM6geveFwsLIcG8XQ/360fx360f",
+   },
+   {
+     id: 9,
+     name: "AK-47 | Neon Rider",
+     status: "Field-Tested",
+     price: 47,
+     image:
+       "https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpot7HxfDhjxszJegJM6dO4q5KCk_LmDLbUkmJE5Ytz0r6U8Y_ziVHn-UY5MT-icIWRJlJoYFnTr1W-lbjrh8Xtu8vAmHY3pGB8sth0zE2w/360fx360f",
+   },
+   {
+     id: 10,
+     name: "Glock-18 | Neo-noir",
+     status: "Minimal Wear",
+     price: 27,
+     image:
+       "https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgposbaqKAxf0Ob3djFN79eJmo-Chcj5Nr_Yg2Yf7pEniL2VoNql0Abt_0VvYW2nJ9PBdQdqMwrWrwC6lbrmjJK7uszOnWwj5Hcq7I0xkg/360fx360f",
+   },
+   {
+     id: 11,
+     name: "AK-47 | Ice coaled",
+     status: "Factory New",
+     price: 19,
+     image:
+       "https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpot7HxfDhjxszJemkV09-5k5SDnvnzIITdn2xZ_ItyiL3Dod6ii1DmqENrMD-nd9OXcAQ5YFHTrFm4w-u915K9v5WczHBrpGB8shQJGVI3/360fx360f",
+   },
+   {
+     id: 12,
+     name: "AWP | Chromatic-Aberration",
+     status: "Field-Tested",
+     price: 10,
+     image:
+       "https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpot621FAR17PLfYQJB5M65mYG0h_76OITck29Y_cg_3ruZo9nziwXj-EBkN2zxdtSRJlRqZFHWqFe3yeu90ZC-v5mYzSFk7j5iuygAeEUWVQ/360fx360f",
+   },
+   {
+     id: 13,
+     name: "M4A4 | Tooth Fairy",
+     status: "Field-Tested",
+     price: 3,
+     image:
+       "https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpou-6kejhjxszFJTwW082jkYWFkfb9IaLum25V4dB8xLqSrY3w3FLj_0A5amqiJtfDcVM_M17T8gK2k7-7jZG16JTBznpn6CU8pSGKsR9Xpxw/360fx360f",
+   },
+   {
+     id: 14,
+     name: "Galil AR | Chromatic-Aberration",
+     status: "Battle-Scarred",
+     price: 2,
+     image:
+       "https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgposbupIgthwczLZAJF7dC_mL-In-X7PrruhmJf4_p5j-jX7ML33Aa3_0RpYW6ictPHIAc6NA7Y81G-kOzv05_tvsyanCBkuiUrsHfdgVXp1mIEZeU0/360fx360f",
+   },
+   {
+     id: 15,
+     name: "Dual-Berettas | Twin Turbo",
+     status: "Field-Tested",
+     price: 2,
+     image:
+       "https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpos7asPwJf0Ob3dShD4OOzmImfkuTLIbrdmnJu5cB1g_zMu9it3gW28ktqMWn7JdSTewI9NwrUqwW3wrrmgZa76Z-fznAy73Im4ynD30vgwyIuzrs/360fx360f",
+   },
+   {
+     id: 16,
+     name: "Glock-18 | Teal-Graf",
+     status: "Factory New",
+     price: 2,
+     image:
+       "https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgposbaqKAxf2-r3fitH_sy3h5O0g_bzNL7VqX9U6clOhuDG_Zi73wzj80Q5NzzyLYWcdlI5ZlnR-VW6xr-5hcS1vZWYmnUwvikntH-Mlgv3308MHHmPJA/360fx360f",
+   },
+   {
+     id: 17,
+     name: "M4A1-S | Night Terror",
+     status: "Factory New",
+     price: 1,
+     image:
+       "https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpou-6kejhz2v_Nfz5H_uOxh7-Gw_alIITYmHhe5ct4i9bN_Iv9nBqyqRE_MmD3JIaVdgFrNwzX_1LrwLjoh8Xu6p3Iy3BnuCEq7XrdmRS-n1gSOUNQ8BIA/360fx360f",
+   },
+
+   {
+     id: 18,
+     name: "M9 Bayonet | Slaughter",
+     status: "Minimal-ware",
+     price: 1948,
+     image:
+       "https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpovbSsLQJf3qr3czxb49KzgL-KmsjuNrnDl1Rd4cJ5nqeZoN6k31Xg80E9Nj_2d9WTcVNrYwyD_AS_l-3rhpS7tM-amHUwvyIi-z-DyOpOogr7/360fx360f",
+   },
+
+   {
+     id: 19,
+     name: "M4A1-S | Hot Rod",
+     status: "Factory-new",
+     price: 1130,
+     image:
+       "https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpou-6kejhz2v_Nfz5H_uO3mr-ZkvPLPu_Qx3hu5Mx2gv2Pp9yn31Li_ERtYW70dYaXdFI8NVvYq1i7xOrohcTt7sudySZnsyNz7GGdwUICPND1TA/360fx360f",
+   },
+
+   {
+     id: 20,
+     name: "Stiletto knife | Slaughter",
+     status: "Minimal-wear",
+     price: 660,
+     image:
+       "https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpovbSsLQJfwOfBfThW-NOJlY20jfL2IbrummJW4NE_37qUo9-hjFHkqEtoMGjyJY6Ve1c5ZFzVrFnryOfs0cK_tZ_PyHJj6D5iuyhfIa0lkA/360fx360f",
+   },
+
+   {
+     id: 21,
+     name: "Flip knife | Slaughter",
+     status: "Factory new",
+     price: 605,
+     image:
+       "https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpovbSsLQJf1f_BYQJD4eOskYKZlsj5Nr_Yg2YfupwkjriUpNWn3Qfj-0M6azr0JIWSewI6YgvRr1fswunogcXtvp7InWwj5HdsdXQfuQ/360fx360f",
+   },
+
+   {
+     id: 22,
+     name: "Classic knife | Slaughter",
+     status: "Field-tested",
+     price: 375,
+     image:
+       "https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpovbSsLQJf0ODbTjxP08azlpKKqPrxN7LEmyVU7pNy0ujDrNX0ilWy8xA9ZGCgcNeSJgE4NFGD_wC6lLvn05K86s7A1zI97XWO0K1Q/360fx360f",
+   },
+
+   {
+     id: 23,
+     name: "Driver gloves | Crimson weaves",
+     status: "Field-tested",
+     price: 321,
+     image:
+       "https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DAX1R3LjtQurWzLhRfwP_BcjZ9_tmyq42Ok_7hPvWAwm1XvJQh07-So9ytjVK1-hdpNmHycICUcAQ3aVvQ-QC3wLvv05Tuot2XnkQ7uFh_/360fx360f",
+   },
+
+   {
+     id: 24,
+     name: "Specialist gloves | Crimson web",
+     status: "Field-tested",
+     price: 317,
+     image:
+       "https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DAQ1h3LAVbv6mxFABs3OXNYgJR_Nm1nYGHnuTgDKzUlHhu-sB1teXI8oThxgLl_0ttZG6gJI_HJA5vYlHQqVjtxei-1JDttJ3Iy3Ji7yJwtivYzBapwUYbqWKMPGU/360fx360f",
+   },
+
+   {
+     id: 25,
+     name: "Sport gloves| Bronze Morph",
+     status: "Minimal-wear",
+     price: 301,
+     image:
+       "https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DAQ1JmMR1osbaqPQJz7ODYfi9W9eOmm4mYmPnLNanekVRD7cFOjfvE8ILKhVGwogYxfT36cIGSdgE_ZQnZ-wW7wr28jZW96prBwHE1vShw5HeJnxDjiEtPZ-BxxavJpsojTz0/360fx360f",
+   },
+
+   {
+     id: 26,
+     name: "Awp| Wildfire",
+     status: "Field-tested",
+     price: 59,
+     image:
+       "https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpot621FAR17PLfYQJV5dCykomZksj4OrzZgiUGsJZ10-yQotTz3AGx_xJuZDj0cISUe1JsZ1zUrlm3kum9jJXt6ZvP1zI97ZbSJAKr/360fx360f",
+   },
+
+   {
+     id: 27,
+     name: "M4A1-S | Hyper Beast",
+     status: "Minimal-wear",
+     price: 56,
+     image:
+       "https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpou-6kejhz2v_Nfz5H_uO1gb-Gw_alDLPIhm5D18d0i_rVyoD8j1yg5UJrNjrwd4SUcQZsZFnR_we3xr3t1pC-uZXMznM37nIr4imMlkOxiRlSLrs4LxeSr5E/360fx360f",
+   },
+
+   {
+     id: 28,
+     name: "USP-S| Neo-Noir",
+     status: "Factory new",
+     price: 38,
+     image:
+       "https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpoo6m1FBRp3_bGcjhQ09-jq5WYh-TLPbTYhFRd4cJ5nqeZrY-l3lbk_RJrZ2H1LIGSJA9rMlmC_wTrxbzvhpTuu5-fzSFg6SJx-z-DyKPJvxFR/360fx360f",
+   },
+   {
+     id: 29,
+     name: "Desert eagle| Code Red",
+     status: "Minimal-wear",
+     price: 35,
+     image:
+       "https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgposr-kLAtl7PTbTjlH7du6kb-KkPDmNqjCmXlu5Mx2gv2PoY6iiQbm_0I-Mmj2d9STcAE3Ml7Y-QTtx-_s08C67cvAziQ2uSAh4mGdwUJMvBFqRQ/360fx360f",
+   },
+
+   {
+     id: 30,
+     name: "CZ75-auto| Xiangliu",
+     status: "Field-tested",
+     price: 12,
+     image:
+       "https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpotaDyfgZf1OD3cid9_9K3n4WYqOfhIavdk1Rc7cF4n-SPoN702FHkqEA6Z22iIIPBJAQ2Y1nQqVe8wLvshJO5vcmbwHVlv3Vx52GdwUJeMGcLoA/360fx360f",
+   },
+
+   {
+     id: 31,
+     name: "AK-47| Head",
+     status: "Field-tested",
+     price: 11,
+     image:
+       "https://community.fastly.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpot7HxfDhjxszJegJK6d2yq5ODmOPLO7TdmVRc7cF4n-SPpomt0Vbm-kFkZ2rzJYbDJ1I4aAuF8we4xL_n0JPqtc6fy3Ix7HZ3sWGdwUJh7QFlnQ/360fx360f",
+   },
+ ];
